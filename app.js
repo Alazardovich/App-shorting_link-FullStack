2aaaf5f224ebfc1c4ed64274aad15d25281d22e2
Artem <lazarazuy@gmail.com>
2022-08-23 13:01:36 +0300

    hide env

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 var cors = require("cors");
-require("dotenv").config();
+require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const PORT = process.env.PORT || 3030;
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/link", require("./routes/link.routes"));
